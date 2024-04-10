f52b3bc447a0bed7e75a64f9755f1cea28b2a735
Steveb175 <123309239+Steveb175@users.noreply.github.com>
2024-04-10 19:49:03 +0000

    basic code to add numbers to input

--- script.js
+++ script.js
@@ -7,17 +7,45 @@ document.addEventListener("keydown", function (event) {
   buttons.forEach((button) => {
     if (
       button.textContent === key ||
-      (key === "Backspace" && button.classList.contains("backspace")) ||
       (key === "c" && button.classList.contains("clear"))
     ) {
       button.classList.add("pressed");
     }
+    else if (
+      key === "Backspace" && button.classList.contains("backspace") 
+    ) {
+      button.classList.add("pressed-delete");
+    }
   });
 });
+
 /* Keyup */
 document.addEventListener("keyup", function (event) {
+  const key = event.key;
   const buttons = document.querySelectorAll(".calculator button");
   buttons.forEach((button) => {
-    button.classList.remove("pressed");
+    if (
+      button.textContent === key ||
+      (key === "c" && button.classList.contains("clear"))
+    ){
+      keydownNumber(event);
+      button.classList.remove("pressed");
+    }
+    else if (
+      key === "Backspace" && button.classList.contains("backspace") 
+    ) {
+      button.classList.remove("pressed-delete");
+    } 
   });
 });
+
+
+/* Calculation */
+/* Function inserts if key pressed is a number */
+function keydownNumber(event) {
+  let result = document.getElementById("result");
+  let pressedKey = event.key;
+  if (!isNaN(pressedKey)) {
+    result.value += pressedKey;
+  }
+}
